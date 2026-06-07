962d012207177b128e64a63682c05b94a4b2005c
P314159i <your-github-email@example.com>
2026-06-07 16:59:49 +0200

    all

--- PushSwap/push_swap.c
+++ PushSwap/push_swap.c
@@ -17,29 +17,89 @@ typedef struct {
 	int top;
 } stack;
 
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (2048);
+	return (0);
+}
+
+
 stack createstack(void)
 {
 	stack a;
 	stack b;
 
-	b.top = -1; 
+	b.top = -1;
 	a.top = -1;
 	return a;
 }
 
-
-
-int main(int argc, char**argv)
+int	valid_input(char *str)
 {
-	int i;
+	if (*str == '+' || *str == '-')
+		str++;
+	if (*str == '\0')
+		return (0);
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			return (0);
+		str++;
+	}
+	return (1);
+}
 
-	i = 0;
-	printf("have %d args:\n", argc);
-	while (i <= argc) //fixxxxxxxxxxxxxxxxxxxxxxxxxxx
-		printf("%s ", argv[i++]);
-	printf("\n");
+int	ft_atoi(const char *nptr, int *error)
+{
+	int		sign;
+	long	intgr;
 
-	return (0); 
+	*error = 0;
+	sign = 1;
+	intgr = 0;
+	while (*nptr == ' ' || *nptr == '\n' || *nptr == '\t'
+		|| *nptr == '\v' || *nptr == '\f' || *nptr == '\r')
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
+	{
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+	}
+	while (ft_isdigit(*nptr))
+	{
+		intgr = intgr * 10 + (*nptr - '0');
+		if ((sign == 1 && intgr > 2147483647)
+			|| (sign == -1 && intgr > 2147483648))
+		{
+			*error = 1;
+			return (0);
+		}
+		nptr++;
+	}
+	return ((int)(intgr * sign));
 }
 
 
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	n;
+	int	error;
+
+	if (argc < 2)
+		return (printf("please enter 2 or more numbers\n"), 1);
+	i = 1;
+	while (i < argc)
+	{
+		if (!valid_input(argv[i]))
+			return (printf("Error\n"), 1);
+		n = ft_atoi(argv[i], &error);
+		if (error)
+			return (printf("Error\n"), 1);
+		i++;
+	}
+	printf("%d\n", n);
+	return (0);
+}
